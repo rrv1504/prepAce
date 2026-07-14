e8d0b722c7711462388bd42e012355a90432ee65
unknown <roshniraichandani294@gmail>
2026-07-14 20:19:07 +0530

    Use absolute Docker path

--- Backend/src/services/dockerService.js
+++ Backend/src/services/dockerService.js
@@ -56,7 +56,7 @@ function executeCodeInDocker(language, folderPath, input = '', timeoutMs = DEFAU
       return reject(error)
     }
 
-    const dockerProcess = spawn('docker', dockerArgs)
+    const dockerProcess = spawn('/usr/bin/docker', dockerArgs)    
     let stdout = ''
     let stderr = ''
     let settled = false
